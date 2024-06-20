b0d9ad3aadc109f55b64117ad667f02f838e5a9f
astroxvim <valerosojamesmartin@gmail.com>
2024-06-21 00:31:40 +0900

    fix: update routes and remove unnecessary prisma contents

--- app/astria/prompt-webhook/route.ts
+++ app/astria/prompt-webhook/route.ts
@@ -1,110 +1,110 @@
 // import prisma from "@/lib/prisma";
-// import { NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 
-// export const dynamic = "force-dynamic";
+export const dynamic = "force-dynamic";
 
-// const appWebhookSecret = process.env.APP_WEBHOOK_SECRET;
+const appWebhookSecret = process.env.APP_WEBHOOK_SECRET;
 
-// if (!appWebhookSecret) {
-//   throw new Error("MISSING APP_WEBHOOK_SECRET!");
-// }
+if (!appWebhookSecret) {
+  throw new Error("MISSING APP_WEBHOOK_SECRET!");
+}
 
-// export async function POST(request: Request) {
-//   type PromptData = {
-//     id: number;
-//     text: string;
-//     negative_prompt: string;
-//     steps: null;
-//     tune_id: number;
-//     trained_at: string;
-//     started_training_at: string;
-//     created_at: string;
-//     updated_at: string;
-//     images: string[];
-//   };
+export async function POST(request: Request) {
+  type PromptData = {
+    id: number;
+    text: string;
+    negative_prompt: string;
+    steps: null;
+    tune_id: number;
+    trained_at: string;
+    started_training_at: string;
+    created_at: string;
+    updated_at: string;
+    images: string[];
+  };
 
-//   const incomingData = (await request.json()) as { prompt: PromptData };
+  const incomingData = (await request.json()) as { prompt: PromptData };
 
-//   const { prompt } = incomingData;
+  const { prompt } = incomingData;
 
-//   console.log({ prompt });
+  console.log({ prompt });
 
-//   const urlObj = new URL(request.url);
-//   const user_id = urlObj.searchParams.get("user_id");
-//   const webhook_secret = urlObj.searchParams.get("webhook_secret");
+  const urlObj = new URL(request.url);
+  const user_id = urlObj.searchParams.get("user_id");
+  const webhook_secret = urlObj.searchParams.get("webhook_secret");
 
-//   if (!webhook_secret) {
-//     return NextResponse.json(
-//       {
-//         message: "Malformed URL, no webhook_secret detected!",
-//       },
-//       { status: 500 }
-//     );
-//   }
+  if (!webhook_secret) {
+    return NextResponse.json(
+      {
+        message: "Malformed URL, no webhook_secret detected!",
+      },
+      { status: 500 }
+    );
+  }
 
-//   console.log(webhook_secret);
+  console.log(webhook_secret);
 
-//   if (webhook_secret.toLowerCase() !== appWebhookSecret?.toLowerCase()) {
-//     return NextResponse.json(
-//       {
-//         message: "Unauthorized!",
-//       },
-//       { status: 401 }
-//     );
-//   }
+  if (webhook_secret.toLowerCase() !== appWebhookSecret?.toLowerCase()) {
+    return NextResponse.json(
+      {
+        message: "Unauthorized!",
+      },
+      { status: 401 }
+    );
+  }
 
-//   console.log(user_id);
+  console.log(user_id);
 
-//   if (!user_id) {
-//     return NextResponse.json(
-//       {
-//         message: "Malformed URL, no user_id detected!",
-//       },
-//       { status: 500 }
-//     );
-//   }
+  if (!user_id) {
+    return NextResponse.json(
+      {
+        message: "Malformed URL, no user_id detected!",
+      },
+      { status: 500 }
+    );
+  }
 
-//   try {
-//     // Here we join all of the arrays into one.
-//     const allHeadshots = prompt.images;
-//     const modelId = prompt.tune_id;
+  try {
+    // Here we join all of the arrays into one.
+    const allHeadshots = prompt.images;
+    const modelId = prompt.tune_id;
 
-//     await Promise.all(
-//       allHeadshots.map(async (image) => {
-//         const newUser = await prisma.images.create({
-//           data: {
-//             gid: user_id,
-//             blob: image
-//           },
-//         });
-//         if (prisma && prisma.imageGeneration) {
-//           const re = await prisma.imageGeneration.findUnique({
-//             where: { gid: user_id },
-//           });
+    await Promise.all(
+      allHeadshots.map(async (image) => {
+        // const newUser = await prisma.images.create({
+        //   data: {
+        //     gid: user_id,
+        //     blob: image
+        //   },
+        // });
+        // if (prisma && prisma.imageGeneration) {
+        //   const re = await prisma.imageGeneration.findUnique({
+        //     where: { gid: user_id },
+        //   });
 
-//           const updatedUser = await prisma.imageGeneration.update({
-//             where: { gid: user_id }, // Use parseInt to convert string to number
-//             data: {
-//               count: (re?.count ?? 1) - 1
-//             },
-//           });
-//         }
-//       })
-//     );
+        //   const updatedUser = await prisma.imageGeneration.update({
+        //     where: { gid: user_id }, // Use parseInt to convert string to number
+        //     data: {
+        //       count: (re?.count ?? 1) - 1
+        //     },
+        //   });
+        // }
+      })
+    );
 
-//     return NextResponse.json(
-//       {
-//         message: "success",
-//       },
-//       { status: 200, statusText: "Success" }
-//     );
-//   } catch (e) {
-//     console.error(e);
-//     return NextResponse.json(
-//       {
-//         message: "Something went wrong!",
-//       },
-//       { status: 500 }
-//     );
-//   }
-// }
+    return NextResponse.json(
+      {
+        message: "success",
+      },
+      { status: 200, statusText: "Success" }
+    );
+  } catch (e) {
+    console.error(e);
+    return NextResponse.json(
+      {
+        message: "Something went wrong!",
+      },
+      { status: 500 }
+    );
+  }
+}
