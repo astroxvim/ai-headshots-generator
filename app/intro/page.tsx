25ab3d1c3a445209a319c59663bca9233c1113fe
astroxvim <valerosojamesmartin@gmail.com>
2024-06-26 23:33:38 +0900

    fix: update intro page style

--- app/intro/page.tsx
+++ app/intro/page.tsx
@@ -22,7 +22,7 @@ const IntroPage = () => {
         className="px:5 absolute inset-x-0 top-3 z-0 h-full w-full transform-gpu overflow-hidden blur-3xl md:right-20 md:h-auto md:w-auto md:px-36"
       >
         <div
-          className="mx-auto aspect-[1155/678] w-[72.1875rem] bg-gradient-to-tr from-[#4FDEFB] to-[#613EE2] opacity-50"
+          className="mx-auto aspect-[1155/678] w-[72.1875rem] bg-gradient-to-tr from-[#1FC5DB] to-[#9A28E6] opacity-30"
           style={{
             clipPath:
               "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
