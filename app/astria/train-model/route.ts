f10113f05123a4a1c10c4284294ca5a5f7a1c22b
astroxvim <valerosojamesmartin@gmail.com>
2024-06-28 03:18:57 +0900

    fix: update route for train model

--- app/astria/train-model/route.ts
+++ app/astria/train-model/route.ts
@@ -21,18 +21,18 @@ if (!appWebhookSecret) {
 export async function POST(request: Request) {
   const payload = await request.json();
   const images = payload.urls;
-  const gender = payload.selectedGender;
-  const option = payload.selectedOption;
+  const gender = payload.gender;
+  const option = payload.option;
   const id = payload.id;
 
 
   // const name = payload.name;
-  const shot = "Close-Up: Focuses closely on the face, capturing fine details and expressions.";
-  const background = "Solid Color: Simple, distraction-free background in a single color.";
-  const light = "Natural: Soft, natural light that mimics daylight, creating a realistic and pleasant effect.";
-  const clothing = "Business Formal: Outfits such as suits and ties, blazers, and formal dresses.";
-  const expression = "Smiling: Adjusts facial features to create a warm, approachable smile.";
-  const colorPalette = "Warm: Uses warm tones such as reds, oranges, and yellows to create a cozy and inviting atmosphere.";
+  // const shot = "Close-Up: Focuses closely on the face, capturing fine details and expressions.";
+  // const background = "Solid Color: Simple, distraction-free background in a single color.";
+  // const light = "Natural: Soft, natural light that mimics daylight, creating a realistic and pleasant effect.";
+  // const clothing = "Business Formal: Outfits such as suits and ties, blazers, and formal dresses.";
+  // const expression = "Smiling: Adjusts facial features to create a warm, approachable smile.";
+  // const colorPalette = "Warm: Uses warm tones such as reds, oranges, and yellows to create a cozy and inviting atmosphere.";
 
 
 
@@ -89,22 +89,22 @@ export async function POST(request: Request) {
         callback: trainWebhookWithParams,
         prompts_attributes:
           // process.env.NEXT_PUBLIC_FALLBACK == "origin" ? 
-          option === PreferenceEnum.StudioMale ?
+          option == PreferenceEnum.StudioMale ?
           [{
             text: `portrait of ohwx ${gender} wearing a business suit, professional photo, white background, Amazing Details, Best Quality, Masterpiece, dramatic lighting, highly detailed, analog photo, overglaze, realistic facial features, natural skin texture, clear eyes, 80mm Sigma f/1.4 or any ZEISS lens`,
             callback: promptWebhookWithParams,
             num_images: parseFloat(process.env.NEXT_PUBLIC_IMAGE_RESULT_COUNT ?? '2') / 2,
-          },] : option === PreferenceEnum.EnvironmentalMale ?
+          }] : option == PreferenceEnum.EnvironmentalMale ?
           [{
             text: `8k close-up linkedin profile picture of ohwx ${gender}, professional business attire, professional headshots, photo-realistic, 4k, high-resolution image, workplace setting, upper body, modern outfit, professional suit, business, blurred background, glass building, office window, high detail, realistic skin texture, soft lighting`,
             callback: promptWebhookWithParams,
             num_images: parseFloat(process.env.NEXT_PUBLIC_IMAGE_RESULT_COUNT ?? '2') / 2,
-          },] : option === PreferenceEnum.StudioFemale ? 
+          }] : option == PreferenceEnum.StudioFemale ? 
           [{
             text: `portrait of ohwx ${gender} wearing a business suit, professional photo, white background, Amazing Details, Best Quality, Masterpiece, dramatic lighting, highly detailed, analog photo, overglaze, realistic facial features, natural skin texture, clear eyes, 80mm Sigma f/1.4 or any ZEISS lens`,
             callback: promptWebhookWithParams,
             num_images: parseFloat(process.env.NEXT_PUBLIC_IMAGE_RESULT_COUNT ?? '2') / 2,
-          },] : [
+          }] : [
             {
               text: `8k close-up linkedin profile picture of ohwx ${gender}, professional business attire, professional headshots, photo-realistic, 4k, high-resolution image, workplace setting, upper body, modern outfit, professional suit, business, blurred background, glass building, office window, high detail, realistic skin texture, soft lighting`,
               callback: promptWebhookWithParams,
