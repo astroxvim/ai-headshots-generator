5368bfc4adb77d07333c031bca7bf8d1876e340f
astroxvim <valerosojamesmartin@gmail.com>
2024-06-29 12:05:35 -0700

    test adding stuio male prompt

--- app/astria/train-model/route.ts
+++ app/astria/train-model/route.ts
@@ -6,7 +6,6 @@ import { NextResponse } from "next/server";
 
 export const dynamic = "force-dynamic";
 
-
 const astriaApiKey = process.env.ASTRIA_API_KEY;
 const astriaTestModeIsOn = process.env.ASTRIA_TEST_MODE === "true";
 // For local development, recommend using an Ngrok tunnel for the domain
@@ -25,17 +24,6 @@ export async function POST(request: Request) {
   const option = payload.option;
   const id = payload.id;
 
-
-  // const name = payload.name;
-  // const shot = "Close-Up: Focuses closely on the face, capturing fine details and expressions.";
-  // const background = "Solid Color: Simple, distraction-free background in a single color.";
-  // const light = "Natural: Soft, natural light that mimics daylight, creating a realistic and pleasant effect.";
-  // const clothing = "Business Formal: Outfits such as suits and ties, blazers, and formal dresses.";
-  // const expression = "Smiling: Adjusts facial features to create a warm, approachable smile.";
-  // const colorPalette = "Warm: Uses warm tones such as reds, oranges, and yellows to create a cozy and inviting atmosphere.";
-
-
-
   const newimageGeneration = await prisma.imageGeneration.create({
     data: {
       gid: id,
@@ -49,7 +37,7 @@ export async function POST(request: Request) {
     return NextResponse.json(
       {
         message:
-          "Missing API Key: Add your Astria API Key to generate headshots",
+        "Missing API Key: Add your Astria API Key to generate headshots",
       },
       {
         status: 500,
@@ -66,108 +54,102 @@ export async function POST(request: Request) {
     );
   }
 
-  // try {
-    const trainWebhook = `https://${process.env.DEPLOYMENT_URL}/astria/train-webhook`;
-    const trainWebhookWithParams = `${trainWebhook}?user_id=${id}&webhook_secret=${appWebhookSecret}`;
+  const trainWebhook = `https://${process.env.DEPLOYMENT_URL}/astria/train-webhook`;
+  const trainWebhookWithParams = `${trainWebhook}?user_id=${id}&webhook_secret=${appWebhookSecret}`;
 
-    const promptWebhook = `https://${process.env.DEPLOYMENT_URL}/astria/prompt-webhook`;
-    const promptWebhookWithParams = `${promptWebhook}?user_id=${id}&webhook_secret=${appWebhookSecret}`;
+  const promptWebhook = `https://${process.env.DEPLOYMENT_URL}/astria/prompt-webhook`;
+  const promptWebhookWithParams = `${promptWebhook}?user_id=${id}&webhook_secret=${appWebhookSecret}`;
 
-    const API_KEY = astriaApiKey;
-    const DOMAIN = "https://api.astria.ai";
+  const API_KEY = astriaApiKey;
+  const DOMAIN = "https://api.astria.ai";
 
-    const body = {
-      tune: {
-        title: "test",
-        // Hard coded tune id of Realistic Vision v5.1 from the gallery - https://www.astria.ai/gallery/tunes
-        // https://www.astria.ai/gallery/tunes/690204/prompts
-        base_tune_id: 690204,
-        name: gender,
-        branch: astriaTestModeIsOn ? "fast" : "sd15",
-        token: "ohwx",
-        image_urls: images,
-        callback: trainWebhookWithParams,
-        prompts_attributes:
-          option == PreferenceEnum.StudioMale ?
-          [{
-            text: `portrait of ohwx ${gender} wearing a business suit, professional photo, white background, color background, Amazing Details, Best Quality, Masterpiece, dramatic lighting, highly detailed, analog photo, overglaze, realistic facial features, natural skin texture, 80mm Sigma f/1.4 or any ZEISS lens`,
+  const imageResultCount = parseFloat(process.env.NEXT_PUBLIC_IMAGE_RESULT_COUNT ?? '1');
+  const numImagesPerPrompt = Math.ceil(imageResultCount / 2);
+
+  const body = {
+    tune: {
+      title: "test",
+      // Hard coded tune id of Realistic Vision v5.1 from the gallery - https://www.astria.ai/gallery/tunes
+      // https://www.astria.ai/gallery/tunes/690204/prompts
+      base_tune_id: 690204,
+      name: gender,
+      branch: astriaTestModeIsOn ? "fast" : "sd15",
+      token: "ohwx",
+      image_urls: images,
+      callback: trainWebhookWithParams,
+      prompts_attributes:
+        option == PreferenceEnum.StudioMale ?
+        [
+          {
+            text: `portrait of ohwx ${gender} wearing a business suit, professional photo, white background, color background, Amazing Details, Best Quality, Masterpiece, dramatic lighting, highly detailed, analog photo, overglaze, realistic facial features, natural skin texture, 80mm Sigma f/1.4 or any ZEISS lens --tiled_upscale`,
             callback: promptWebhookWithParams,
-            num_images: parseFloat(process.env.NEXT_PUBLIC_IMAGE_RESULT_COUNT ?? '1'),
-          }] : option == PreferenceEnum.EnvironmentalMale ?
-          [{
+            num_images: numImagesPerPrompt,
+          },
+          {
+            text: `8k close up linkedin profile picture of handsome ohwx ${gender}, buttoned black shirt, warm skin tones colors --tiled_upscale`,
+            negative_prompt: 'old, wrinkles, eye bags, mole, blemish, scar, sad, severe, 3d, cg',
+            callback: promptWebhookWithParams,
+            num_images: numImagesPerPrompt,
+          }
+        ] : option == PreferenceEnum.EnvironmentalMale ?
+        [{
+          text: `8k close-up linkedin profile picture of ohwx ${gender}, professional business attire, professional headshots, photo-realistic, 4k, high-resolution image, workplace setting, upper body, modern outfit, professional suit, business, blurred background, glass building, office window, high detail, realistic skin texture, soft lighting`,
+          callback: promptWebhookWithParams,
+          num_images: imageResultCount,
+        }] : option == PreferenceEnum.StudioFemale ? 
+        [{
+          text: `portrait of ohwx ${gender} wearing a business suit, professional photo, white background, color background, Amazing Details, Best Quality, Masterpiece, dramatic lighting, highly detailed, analog photo, overglaze, realistic facial features, natural skin texture, 80mm Sigma f/1.4 or any ZEISS lens`,
+          callback: promptWebhookWithParams,
+          num_images: imageResultCount,
+        }] : option == PreferenceEnum.EnvironmentalFemale ? 
+        [{
+          text: `8k close-up linkedin profile picture of ohwx ${gender}, professional business attire, professional headshots, photo-realistic, 4k, high-resolution image, workplace setting, upper body, modern outfit, professional suit, business, blurred background, glass building, office window, high detail, realistic skin texture, soft lighting`,
+          callback: promptWebhookWithParams,
+          num_images: imageResultCount,
+        }] : [
+          {
             text: `8k close-up linkedin profile picture of ohwx ${gender}, professional business attire, professional headshots, photo-realistic, 4k, high-resolution image, workplace setting, upper body, modern outfit, professional suit, business, blurred background, glass building, office window, high detail, realistic skin texture, soft lighting`,
             callback: promptWebhookWithParams,
-            num_images: parseFloat(process.env.NEXT_PUBLIC_IMAGE_RESULT_COUNT ?? '1'),
-          }] : option == PreferenceEnum.StudioFemale ? 
-          [{
-            text: `portrait of ohwx ${gender} wearing a business suit, professional photo, white background, color background, Amazing Details, Best Quality, Masterpiece, dramatic lighting, highly detailed, analog photo, overglaze, realistic facial features, natural skin texture, 80mm Sigma f/1.4 or any ZEISS lens`,
-            callback: promptWebhookWithParams,
-            num_images: parseFloat(process.env.NEXT_PUBLIC_IMAGE_RESULT_COUNT ?? '1'),
-          }] : [
-            {
-              text: `8k close-up linkedin profile picture of ohwx ${gender}, professional business attire, professional headshots, photo-realistic, 4k, high-resolution image, workplace setting, upper body, modern outfit, professional suit, business, blurred background, glass building, office window, high detail, realistic skin texture, soft lighting`,
-              callback: promptWebhookWithParams,
-              num_images: parseFloat(process.env.NEXT_PUBLIC_IMAGE_RESULT_COUNT ?? '1'),
-            }
-          ]
-        
-      },
-    };
+            num_images: imageResultCount,
+          }
+        ]
+    },
+  };
 
-    const response = await axios.post(DOMAIN + "/tunes", body, {
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${API_KEY}`,
-      },
-    });
+  const response = await axios.post(DOMAIN + "/tunes", body, {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${API_KEY}`,
+    },
+  });
 
+  const { status, statusText, data: tune } = response;
 
-    const { status, statusText, data: tune } = response;
-
-    if (status !== 201) {
-      console.error({ status });
-      if (status === 400) {
-        return NextResponse.json(
-          {
-            message: "webhookUrl must be a URL address",
-          },
-          { status }
-        );
-      }
-      if (status === 402) {
-        return NextResponse.json(
-          {
-            message: "Training models is only available on paid plans.",
-          },
-          { status }
-        );
-      }
-
-      
-
+  if (status !== 201) {
+    console.error({ status });
+    if (status === 400) {
+      return NextResponse.json(
+        {
+          message: "webhookUrl must be a URL address",
+        },
+        { status }
+      );
     }
+    if (status === 402) {
+      return NextResponse.json(
+        {
+          message: "Training models is only available on paid plans.",
+        },
+        { status }
+      );
+    }
+  }
 
-    return NextResponse.json(
-      {
-        message: "success",
-        data: tune,
-      },
-      { status: 200 },
-    );
-  // } catch (e) {
-  //   console.error(e);
-  //   return NextResponse.json(
-  //     {
-  //       message: "Something went wrong!",
-  //     },
-  //     { status: 500 }
-  //   );
-  // }
-
-  // return NextResponse.json(
-  //   {
-  //     message: "success",
-  //   },
-  //   { status: 200 }
-  // );
+  return NextResponse.json(
+    {
+      message: "success",
+      data: tune,
+    },
+    { status: 200 }
+  );
 }
