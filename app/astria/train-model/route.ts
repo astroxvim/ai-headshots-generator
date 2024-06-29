74fd16a2c264affc431cea3e8bc8936d67f5ac94
astroxvim <valerosojamesmartin@gmail.com>
2024-06-29 14:26:08 -0700

    prompt test

--- app/astria/train-model/route.ts
+++ app/astria/train-model/route.ts
@@ -1,6 +1,7 @@
 import { PreferenceEnum } from "@/app/constants/preference-types";
 import prisma from "@/lib/prisma";
 import axios from "axios";
+import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
 
 export const dynamic = "force-dynamic";
@@ -8,6 +9,7 @@ export const dynamic = "force-dynamic";
 const astriaApiKey = process.env.ASTRIA_API_KEY;
 const astriaTestModeIsOn = process.env.ASTRIA_TEST_MODE === "true";
 const appWebhookSecret = process.env.APP_WEBHOOK_SECRET;
+const stripeIsConfigured = process.env.NEXT_PUBLIC_STRIPE_IS_ENABLED === "true";
 
 if (!appWebhookSecret) {
   throw new Error("MISSING APP_WEBHOOK_SECRET!");
@@ -20,7 +22,7 @@ export async function POST(request: Request) {
   const option = payload.option;
   const id = payload.id;
 
-  const newImageGeneration = await prisma.imageGeneration.create({
+  const newimageGeneration = await prisma.imageGeneration.create({
     data: {
       gid: id,
       blobUrls: images,
@@ -34,7 +36,9 @@ export async function POST(request: Request) {
       {
         message: "Missing API Key: Add your Astria API Key to generate headshots",
       },
-      { status: 500 }
+      {
+        status: 500,
+      }
     );
   }
 
@@ -56,6 +60,10 @@ export async function POST(request: Request) {
   const API_KEY = astriaApiKey;
   const DOMAIN = "https://api.astria.ai";
 
+  const numImagesPerPrompt = Math.ceil(
+    parseFloat(process.env.NEXT_PUBLIC_IMAGE_RESULT_COUNT ?? "1") / 2
+  );
+
   const body = {
     tune: {
       title: "test",
@@ -66,7 +74,7 @@ export async function POST(request: Request) {
       image_urls: images,
       callback: trainWebhookWithParams,
       prompts_attributes:
-        option === PreferenceEnum.StudioMale
+        option == PreferenceEnum.StudioMale
           ? [
               {
                 text: `portrait of ohwx ${gender} wearing a business suit, professional photo, white background, color background, Amazing Details, Best Quality, Masterpiece, dramatic lighting, highly detailed, analog photo, overglaze, realistic facial features, natural skin texture, 80mm Sigma f/1.4 or any ZEISS lens`,
@@ -74,39 +82,36 @@ export async function POST(request: Request) {
                 num_images: 4,
               },
               {
-                text: `8k close up linkedin profile picture of handsome ohwx ${gender}, buttoned black shirt, warm skin tones colors --tiled_upscale`,
+                text: `8k close up linkedin profile picture of handsome ohwx ${gender}, buttoned black shirt, warm skin tones colors`,
                 callback: promptWebhookWithParams,
                 num_images: 4,
               },
             ]
-          : option === PreferenceEnum.EnvironmentalMale
+          : option == PreferenceEnum.EnvironmentalMale
           ? [
               {
                 text: `8k close-up linkedin profile picture of ohwx ${gender}, professional business attire, professional headshots, photo-realistic, 4k, high-resolution image, workplace setting, upper body, modern outfit, professional suit, business, blurred background, glass building, office window, high detail, realistic skin texture, soft lighting`,
                 callback: promptWebhookWithParams,
-                num_images: 8,
+                num_images: parseFloat(process.env.NEXT_PUBLIC_IMAGE_RESULT_COUNT ?? "1"),
               },
             ]
-          : option === PreferenceEnum.StudioFemale
+          : option == PreferenceEnum.StudioFemale
           ? [
               {
                 text: `portrait of ohwx ${gender} wearing a business suit, professional photo, white background, color background, Amazing Details, Best Quality, Masterpiece, dramatic lighting, highly detailed, analog photo, overglaze, realistic facial features, natural skin texture, 80mm Sigma f/1.4 or any ZEISS lens`,
                 callback: promptWebhookWithParams,
-                num_images: 8,
+                num_images: parseFloat(process.env.NEXT_PUBLIC_IMAGE_RESULT_COUNT ?? "1"),
               },
             ]
           : [
               {
                 text: `8k close-up linkedin profile picture of ohwx ${gender}, professional business attire, professional headshots, photo-realistic, 4k, high-resolution image, workplace setting, upper body, modern outfit, professional suit, business, blurred background, glass building, office window, high detail, realistic skin texture, soft lighting`,
                 callback: promptWebhookWithParams,
-                num_images: 8,
+                num_images: parseFloat(process.env.NEXT_PUBLIC_IMAGE_RESULT_COUNT ?? "1"),
               },
             ],
     },
   };
-
-  // Debugging log to ensure the structure is correct
-  console.log("Request Body:", JSON.stringify(body, null, 2));
 
   try {
     const response = await axios.post(DOMAIN + "/tunes", body, {
@@ -116,16 +121,26 @@ export async function POST(request: Request) {
       },
     });
 
-    const { status, data: tune } = response;
+    const { status, statusText, data: tune } = response;
 
     if (status !== 201) {
       console.error({ status });
-      return NextResponse.json(
-        {
-          message: "Unexpected response status",
-        },
-        { status }
-      );
+      if (status === 400) {
+        return NextResponse.json(
+          {
+            message: "webhookUrl must be a URL address",
+          },
+          { status }
+        );
+      }
+      if (status === 402) {
+        return NextResponse.json(
+          {
+            message: "Training models is only available on paid plans.",
+          },
+          { status }
+        );
+      }
     }
 
     return NextResponse.json(
@@ -136,16 +151,10 @@ export async function POST(request: Request) {
       { status: 200 }
     );
   } catch (e) {
-    console.error("Axios Error:", e);
-    if (e.response) {
-      console.error("Response Data:", e.response.data);
-      console.error("Response Status:", e.response.status);
-      console.error("Response Headers:", e.response.headers);
-    }
+    console.error(e);
     return NextResponse.json(
       {
         message: "Something went wrong!",
-        error: e.message,
       },
       { status: 500 }
     );
